e159fcdc4e1dd53d82ee8e4894b4ca45a03f0485
古明扬 <gmy@jwzg.com>
2022-06-27 14:49:22 +0800

    ch03

--- gin_start/ch03/main.go
+++ gin_start/ch03/main.go
@@ -7,15 +7,28 @@ func main() {
 	router := gin.Default()
 	goodsGroup := router.Group("/goods")
 	{
-		goodsGroup.GET("/list", goodsList)
-		goodsGroup.GET("/login", goodsLogin)
-		goodsGroup.POST("/read", goodsRead)
+		//goodsGroup.GET("", goodsList)
+		//goodsGroup.GET("", goodsLogin)
+		//goodsGroup.GET("", goodsRead)
+		//goodsGroup.GET("/1", goodsDetail) 设置一个变量
+		goodsGroup.GET("/:id/:action", goodsDetail)
 	}
 	router.Run()
 }
 
-func goodsRead(context *gin.Context) {
+func goodsDetail(context *gin.Context) {
+	id := context.Param("id")
+	action := context.Param("action")
+	context.JSON(200, gin.H{
+		"id":     id,
+		"aciton": action,
+	})
+}
 
+func goodsRead(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"name": "goodsRead",
+	})
 }
 
 func goodsLogin(context *gin.Context) {
